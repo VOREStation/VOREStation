TrongleCo/sealab13

refactors admin faxes to allow for more simple to use fax creation and replying. Admins also gain the ability to initiate faxes. And mods can send and receive faxes now.
Former-commit-id: 87d85b3f9003d8352b58da79207c49eba44accce

code/modules/admin/admin_verbs.dm
@@ -92,7 +92,8 @@ var/list/admin_verbs_admin = list(
 	/client/proc/fixatmos,
 	/client/proc/list_traders,
 	/client/proc/add_trader,
-	/client/proc/remove_trader
+	/client/proc/remove_trader,
+	/datum/admins/proc/sendFax
 )
 var/list/admin_verbs_ban = list(
 	/client/proc/unban_panel,
@@ -302,7 +303,9 @@ var/list/admin_verbs_mod = list(
 	/client/proc/jobbans,
 	/client/proc/cmd_admin_subtle_message, // send an message to somebody as a 'voice in their head',
 	/client/proc/aooc,
-	/datum/admins/proc/paralyze_mob
+	/datum/admins/proc/paralyze_mob,
+	/datum/admins/proc/sendFax
+
 )
 
 var/list/admin_verbs_mentor = list(

VOREStation/VOREStation

code/modules/admin/admin_verbs.dm
@@ -96,7 +96,8 @@ var/list/admin_verbs_admin = list(
 	/client/proc/toggle_debug_logs,
 	/client/proc/toggle_attack_logs,
 	/datum/admins/proc/paralyze_mob,
-	/client/proc/fixatmos
+	/client/proc/fixatmos,
+	/datum/admins/proc/sendFax
 )
 var/list/admin_verbs_ban = list(
 	/client/proc/unban_panel,
@@ -305,7 +306,9 @@ var/list/admin_verbs_mod = list(
 	/client/proc/cmd_admin_subtle_message, 	//send an message to somebody as a 'voice in their head',
 	/datum/admins/proc/paralyze_mob,
 	/client/proc/cmd_admin_direct_narrate,
-	/client/proc/allow_character_respawn    // Allows a ghost to respawn ,
+	/client/proc/allow_character_respawn,   // Allows a ghost to respawn ,
+	/datum/admins/proc/sendFax
+
 )
 
 var/list/admin_verbs_mentor = list(