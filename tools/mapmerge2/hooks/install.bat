ae08a6896f07ef20b17ae776fed90e61c9174de3
Arokha Sieyes <arokha@arokha.com>
2018-01-27 23:38:52 -0500

    Add MapMerge2 from /tg/

---
+++ tools/mapmerge2/hooks/install.bat
@@ -0,0 +1,15 @@
+@echo off
+cd %~dp0
+for %%f in (*.hook) do (
+	echo Installing hook: %%~nf
+	mkdir ..\..\..\.git\hooks
+	copy %%f ..\..\..\.git\hooks\%%~nf >nul
+)
+for %%f in (*.merge) do (
+	echo Installing merge driver: %%~nf
+	echo [merge "%%~nf"]^
+
+	driver = tools/hooks/%%f %%P %%O %%A %%B %%L >> ..\..\..\.git\config
+)
+echo Done
+pause
