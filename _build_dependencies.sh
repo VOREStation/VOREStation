78fdff73516addc059cc68cf462a3f91a1730a89
Atermonera <atermonera@gmail.com>
2022-03-29 23:07:14 -0800

    updates byond major.minor and dmm suite versions

--- _build_dependencies.sh
+++ _build_dependencies.sh
@@ -1,11 +1,15 @@
 # This file has all the information on what versions of libraries are thrown into the code
 # For dreamchecker
-export SPACEMAN_DMM_VERSION=suite-1.7
+export SPACEMAN_DMM_VERSION=suite-1.7.2
 # For NanoUI + TGUI
 export NODE_VERSION=12
 # Byond Major
 export BYOND_MAJOR=514
 # Byond Minor
+<<<<<<< HEAD
 export BYOND_MINOR=1569
+=======
+export BYOND_MINOR=1575
+>>>>>>> 21ee12ae4b1... Merge pull request #8472 from Spookerton/spkrtn/sys/target-updates
 # Macro Count
 export MACRO_COUNT=4
