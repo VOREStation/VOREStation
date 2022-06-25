6b078a1d6b1dbc17555145df612d2471003ecdea
ItsSelis <selis@xynolabs.com>
2022-06-25 22:11:38 +0200

    Rebuilt yarn.lock

--- tools/ci/install_build_deps.sh
+++ tools/ci/install_build_deps.sh
@@ -6,7 +6,7 @@ source _build_dependencies.sh
 source ~/.nvm/nvm.sh
 nvm install $NODE_VERSION
 nvm use $NODE_VERSION
-npm install --global yarn
+npm install --location=global yarn
 
 pip install --user PyYaml -q
 pip install --user beautifulsoup4 -q
