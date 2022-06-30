569e5a856516c751aae4219ffeca6b0abcc1a6f1
ItsSelis <selis@xynolabs.com>
2022-06-30 21:26:41 +0200

    Moves simple ntos interfaces to .tsx

--- tgui/packages/tgui/interfaces/NtosShutoffMonitor.tsx
+++ tgui/packages/tgui/interfaces/NtosShutoffMonitor.tsx
@@ -1,7 +1,7 @@
 import { NtosWindow } from '../layouts';
 import { ShutoffMonitorContent } from './ShutoffMonitor';
 
-export const NtosShutoffMonitor = (props, context) => {
+export const NtosShutoffMonitor = () => {
   return (
     <NtosWindow width={627} height={700} resizable>
       <NtosWindow.Content>
