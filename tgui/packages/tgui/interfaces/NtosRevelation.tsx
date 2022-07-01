79ec91a174acbf14277af01846ad4b0470651e8a
ItsSelis <selis@xynolabs.com>
2022-07-01 23:41:33 +0200

    Testing with a bundle change

--- tgui/packages/tgui/interfaces/NtosRevelation.tsx
+++ tgui/packages/tgui/interfaces/NtosRevelation.tsx
@@ -1,9 +1,15 @@
 import { Section, Button, LabeledList } from '../components';
 import { useBackend } from '../backend';
 import { NtosWindow } from '../layouts';
+import { BooleanLike } from 'common/react';
+
+type Data = {
+  armed: BooleanLike;
+};
 
 export const NtosRevelation = (props, context) => {
-  const { act, data } = useBackend(context);
+  const { act, data } = useBackend<Data>(context);
+  const { armed } = data;
   return (
     <NtosWindow width={400} height={250} theme="syndicate">
       <NtosWindow.Content>
@@ -23,14 +29,14 @@ export const NtosRevelation = (props, context) => {
               label="Payload Status"
               buttons={
                 <Button
-                  content={data.armed ? 'ARMED' : 'DISARMED'}
-                  color={data.armed ? 'bad' : 'average'}
+                  content={armed ? 'ARMED' : 'DISARMED'}
+                  color={armed ? 'bad' : 'average'}
                   onClick={() => act('PRG_arm')}
                 />
               }
             />
           </LabeledList>
-          <Button fluid bold content="ACTIVATE" textAlign="center" color="bad" disabled={!data.armed} />
+          <Button fluid bold content="ACTIVATE" textAlign="center" color="bad" disabled={!armed} />
         </Section>
       </NtosWindow.Content>
     </NtosWindow>
