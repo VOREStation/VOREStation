41b202782fc7568700bf6e2af66d7b3b2d195554
Atermonera <atermonera@gmail.com>
2022-07-18 22:39:51 -0800

    updated CI byond target to 1585

--- _build_dependencies.sh
+++ _build_dependencies.sh
@@ -6,6 +6,10 @@ export NODE_VERSION=16
 # Byond Major
 export BYOND_MAJOR=514
 # Byond Minor
+<<<<<<< HEAD
 export BYOND_MINOR=1569
+=======
+export BYOND_MINOR=1585
+>>>>>>> ca13a811416... Merge pull request #8669 from Spookerton/spkrtn/sys/tests-1585
 # Macro Count
 export MACRO_COUNT=4
