9e36f66d899987c277591402f7572b8d5dd9a0b5
ItsSelis <selis@xynolabs.com>
2023-06-06 12:30:45 +0200

    Prettify interface file

--- tgui/packages/tgui/interfaces/TicketsPanel.tsx
+++ tgui/packages/tgui/interfaces/TicketsPanel.tsx
@@ -59,11 +59,19 @@ type Ticket = {
   selected: BooleanLike;
 };
 
-const getFilteredTickets = (tickets: Ticket[], state: string, level: number): Ticket[] => {
+const getFilteredTickets = (
+  tickets: Ticket[],
+  state: string,
+  level: number
+): Ticket[] => {
   let result: Ticket[] = [];
 
   tickets.forEach((t) => {
-    if ((t.state === state || state === 'all') && (t.level === level || level === 2)) result.push(t);
+    if (
+      (t.state === state || state === 'all') &&
+      (t.level === level || level === 2)
+    )
+      result.push(t);
   });
 
   return result;
@@ -73,8 +81,16 @@ export const TicketsPanel = (props, context) => {
   const { act, data } = useBackend<Data>(context);
   const { tickets, selected_ticket } = data;
 
-  const [stateFilter, setStateFilter] = useLocalState(context, 'stateFilter', 'open');
-  const [levelFilter, setLevelFilter] = useLocalState(context, 'levelFilter', 2);
+  const [stateFilter, setStateFilter] = useLocalState(
+    context,
+    'stateFilter',
+    'open'
+  );
+  const [levelFilter, setLevelFilter] = useLocalState(
+    context,
+    'levelFilter',
+    2
+  );
 
   let filtered_tickets = getFilteredTickets(tickets, stateFilter, levelFilter);
   return (
@@ -88,7 +104,11 @@ export const TicketsPanel = (props, context) => {
                 maxHeight="160px"
                 options={Object.values(State)}
                 selected={State[stateFilter]}
-                onSelected={(val) => setStateFilter(Object.keys(State)[Object.values(State).indexOf(val)])}
+                onSelected={(val) =>
+                  setStateFilter(
+                    Object.keys(State)[Object.values(State).indexOf(val)]
+                  )
+                }
               />
               <Divider />
               <Dropdown
@@ -96,7 +116,9 @@ export const TicketsPanel = (props, context) => {
                 maxHeight="160px"
                 options={Object.values(Level)}
                 selected={Level[levelFilter]}
-                onSelected={(val) => setLevelFilter(Object.values(Level).indexOf(val))}
+                onSelected={(val) =>
+                  setLevelFilter(Object.values(Level).indexOf(val))
+                }
               />
             </Section>
             <Section title="Tickets" scrollable>
@@ -110,13 +132,20 @@ export const TicketsPanel = (props, context) => {
                   <Tabs.Tab
                     key={ticket.id}
                     selected={ticket.id === selected_ticket?.id}
-                    onClick={() => act('pick_ticket', { ticket_id: ticket.id })}>
+                    onClick={() =>
+                      act('pick_ticket', { ticket_id: ticket.id })
+                    }>
                     <Box inline>
                       <Box>
-                        <Button content={Level[ticket.level]} color={LevelColor[ticket.level]} /> {ticket.name}
+                        <Button
+                          content={Level[ticket.level]}
+                          color={LevelColor[ticket.level]}
+                        />{' '}
+                        {ticket.name}
                       </Box>
                       <Box fontSize={0.9} textColor={StateColor[ticket.state]}>
-                        State: {State[ticket.state]} | Assignee: {ticket.handler}
+                        State: {State[ticket.state]} | Assignee:{' '}
+                        {ticket.handler}
                       </Box>
                     </Box>
                   </Tabs.Tab>
@@ -130,36 +159,70 @@ export const TicketsPanel = (props, context) => {
                 title={'Ticket #' + selected_ticket.id}
                 buttons={
                   <Box nowrap>
-                    <Button icon="arrow-up" content="Undock" onClick={() => act('undock_ticket')} />{' '}
-                    <Button icon="pen" content="Rename Ticket" onClick={() => act('retitle_ticket')} />{' '}
+                    <Button
+                      icon="arrow-up"
+                      content="Undock"
+                      onClick={() => act('undock_ticket')}
+                    />{' '}
+                    <Button
+                      icon="pen"
+                      content="Rename Ticket"
+                      onClick={() => act('retitle_ticket')}
+                    />{' '}
                     <Button content="Legacy UI" onClick={() => act('legacy')} />{' '}
-                    <Button content={Level[selected_ticket.level]} color={LevelColor[selected_ticket.level]} />
+                    <Button
+                      content={Level[selected_ticket.level]}
+                      color={LevelColor[selected_ticket.level]}
+                    />
                   </Box>
                 }>
                 <LabeledList>
                   <LabeledList.Item label="Ticket ID">
-                    #{selected_ticket.id}: <div dangerouslySetInnerHTML={{ __html: selected_ticket.name }} />
+                    #{selected_ticket.id}:{' '}
+                    <div
+                      dangerouslySetInnerHTML={{ __html: selected_ticket.name }}
+                    />
                   </LabeledList.Item>
-                  <LabeledList.Item label="Type">{Level[selected_ticket.level]}</LabeledList.Item>
-                  <LabeledList.Item label="State">{State[selected_ticket.state]}</LabeledList.Item>
-                  <LabeledList.Item label="Assignee">{selected_ticket.handler}</LabeledList.Item>
+                  <LabeledList.Item label="Type">
+                    {Level[selected_ticket.level]}
+                  </LabeledList.Item>
+                  <LabeledList.Item label="State">
+                    {State[selected_ticket.state]}
+                  </LabeledList.Item>
+                  <LabeledList.Item label="Assignee">
+                    {selected_ticket.handler}
+                  </LabeledList.Item>
                   {State[selected_ticket.state] === State.open ? (
                     <LabeledList.Item label="Opened At">
-                      {selected_ticket.opened_at_date} ({Math.round((selected_ticket.opened_at / 600) * 10) / 10}{' '}
+                      {selected_ticket.opened_at_date} (
+                      {Math.round((selected_ticket.opened_at / 600) * 10) / 10}{' '}
                       minutes ago.)
                     </LabeledList.Item>
                   ) : (
                     <LabeledList.Item label="Closed At">
-                      {selected_ticket.closed_at_date} ({Math.round((selected_ticket.closed_at / 600) * 10) / 10}{' '}
-                      minutes ago.) <Button content="Reopen" onClick={() => act('reopen_ticket')} />
+                      {selected_ticket.closed_at_date} (
+                      {Math.round((selected_ticket.closed_at / 600) * 10) / 10}{' '}
+                      minutes ago.){' '}
+                      <Button
+                        content="Reopen"
+                        onClick={() => act('reopen_ticket')}
+                      />
                     </LabeledList.Item>
                   )}
                   <LabeledList.Item label="Actions">
-                    <div dangerouslySetInnerHTML={{ __html: selected_ticket.actions }} />
+                    <div
+                      dangerouslySetInnerHTML={{
+                        __html: selected_ticket.actions,
+                      }}
+                    />
                   </LabeledList.Item>
                   <LabeledList.Item label="Log">
                     {Object.keys(selected_ticket.log).map((L) => (
-                      <Box dangerouslySetInnerHTML={{ __html: selected_ticket.log[L] }} />
+                      <Box
+                        dangerouslySetInnerHTML={{
+                          __html: selected_ticket.log[L],
+                        }}
+                      />
                     ))}
                   </LabeledList.Item>
                 </LabeledList>
@@ -169,8 +232,18 @@ export const TicketsPanel = (props, context) => {
                 title="No ticket selected"
                 buttons={
                   <Box nowrap>
-                    <Button disabled icon="arrow-up" content="Undock" onClick={() => act('undock_ticket')} />{' '}
-                    <Button disabled icon="pen" content="Rename Ticket" onClick={() => act('retitle_ticket')} />{' '}
+                    <Button
+                      disabled
+                      icon="arrow-up"
+                      content="Undock"
+                      onClick={() => act('undock_ticket')}
+                    />{' '}
+                    <Button
+                      disabled
+                      icon="pen"
+                      content="Rename Ticket"
+                      onClick={() => act('retitle_ticket')}
+                    />{' '}
                     <Button content="Legacy UI" onClick={() => act('legacy')} />
                   </Box>
                 }>
