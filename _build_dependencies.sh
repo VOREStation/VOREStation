d212c296333b5b79daef8a3ca594850745618fdb
Selis <selis@xynolabs.com>
2024-02-16 18:13:52 +0100

    Build dependencies at 514

--- _build_dependencies.sh
+++ _build_dependencies.sh
@@ -4,8 +4,8 @@ export SPACEMAN_DMM_VERSION=suite-1.7
 # For NanoUI + TGUI
 export NODE_VERSION=16
 # Byond Major
-export BYOND_MAJOR=515
+export BYOND_MAJOR=514
 # Byond Minor
-export BYOND_MINOR=1620
+export BYOND_MINOR=1589
 # Macro Count
 export MACRO_COUNT=4
