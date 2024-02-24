fca87909a99d27b80ffab01dff401dee51cf17c7
Kashargul <144968721+Kashargul@users.noreply.github.com>
2024-02-24 14:15:36 +0100

    quick fix NTOS crew monitor

--- tgui/packages/tgui/interfaces/NtosCrewMonitor.tsx
+++ tgui/packages/tgui/interfaces/NtosCrewMonitor.tsx
@@ -1,11 +1,28 @@
+import { useState } from 'react';
+
 import { NtosWindow } from '../layouts';
 import { CrewMonitorContent } from './CrewMonitor';
 
 export const NtosCrewMonitor = () => {
+  const [tabIndex, setTabIndex] = useState(0);
+  const [zoom, setZoom] = useState(1);
+
+  function handleTabIndex(value) {
+    setTabIndex(value);
+  }
+
+  function handleZoom(value) {
+    setZoom(value);
+  }
   return (
     <NtosWindow width={800} height={600}>
       <NtosWindow.Content>
-        <CrewMonitorContent />
+        <CrewMonitorContent
+          tabIndex={tabIndex}
+          zoom={zoom}
+          onTabIndex={handleTabIndex}
+          onZoom={handleZoom}
+        />
       </NtosWindow.Content>
     </NtosWindow>
   );
