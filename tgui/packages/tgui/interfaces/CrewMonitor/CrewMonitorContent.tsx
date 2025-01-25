584b045e0e1297f9a66bcbf9eb8203dd7c8117ea
Kashargul <144968721+Kashargul@users.noreply.github.com>
2025-01-25 17:03:10 +0100

    Adds filters and sorting to crewmonitor (#16915)
    * Adds filters and sorting to crewmonitor
    
    * show active

---
+++ tgui/packages/tgui/interfaces/CrewMonitor/CrewMonitorContent.tsx
@@ -0,0 +1,57 @@
+import { sortBy } from 'common/collections';
+import { flow } from 'common/fp';
+import { useBackend } from 'tgui/backend';
+import { Box, Icon, Tabs } from 'tgui/components';
+
+import { CrewMonitorCrew } from './CrewMonitorCrew';
+import { CrewMonitorMapView } from './CrewMonitorMapView';
+import { crewmember, Data } from './types';
+
+export const CrewMonitorContent = (props: {
+  tabIndex: number;
+  zoom: number;
+  onTabIndex: Function;
+  onZoom: Function;
+}) => {
+  const { data } = useBackend<Data>();
+
+  const { crewmembers = [] } = data;
+
+  const crew: crewmember[] = flow([
+    (crewmembers: crewmember[]) => sortBy(crewmembers, (cm) => cm.name),
+    (crewmembers: crewmember[]) => sortBy(crewmembers, (cm) => cm?.x),
+    (crewmembers: crewmember[]) => sortBy(crewmembers, (cm) => cm?.y),
+    (crewmembers: crewmember[]) => sortBy(crewmembers, (cm) => cm?.realZ),
+  ])(crewmembers);
+
+  const tab: React.JSX.Element[] = [];
+  // Data view
+  // Please note, if you ever change the zoom values,
+  // you MUST update styles/components/Tooltip.scss
+  // and change the @for scss to match.
+  tab[0] = <CrewMonitorCrew crew={crew} />;
+
+  tab[1] = <CrewMonitorMapView zoom={props.zoom} onZoom={props.onZoom} />;
+
+  return (
+    <>
+      <Tabs>
+        <Tabs.Tab
+          key="DataView"
+          selected={0 === props.tabIndex}
+          onClick={() => props.onTabIndex(0)}
+        >
+          <Icon name="table" /> Data View
+        </Tabs.Tab>
+        <Tabs.Tab
+          key="MapView"
+          selected={1 === props.tabIndex}
+          onClick={() => props.onTabIndex(1)}
+        >
+          <Icon name="map-marked-alt" /> Map View
+        </Tabs.Tab>
+      </Tabs>
+      <Box m={2}>{tab[props.tabIndex] || <Box textColor="red">ERROR</Box>}</Box>
+    </>
+  );
+};
