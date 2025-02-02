c2e108b8483d9c5c06fb91be98acfb5abf67ecde
Kashargul <144968721+Kashargul@users.noreply.github.com>
2025-02-02 03:29:20 +0100

    one less

--- tgui/packages/tgui/interfaces/CrewMonitor/CrewMonitorContent.tsx
+++ tgui/packages/tgui/interfaces/CrewMonitor/CrewMonitorContent.tsx
@@ -1,11 +1,9 @@
-import { sortBy } from 'common/collections';
 import { useBackend } from 'tgui/backend';
 import { Box, Icon, Tabs } from 'tgui-core/components';
-import { flow } from 'tgui-core/fp';
 
 import { CrewMonitorCrew } from './CrewMonitorCrew';
 import { CrewMonitorMapView } from './CrewMonitorMapView';
-import { crewmember, Data } from './types';
+import { Data } from './types';
 
 export const CrewMonitorContent = (props: {
   tabIndex: number;
@@ -17,19 +15,12 @@ export const CrewMonitorContent = (props: {
 
   const { crewmembers = [] } = data;
 
-  const crew: crewmember[] = flow([
-    (crewmembers: crewmember[]) => sortBy(crewmembers, (cm) => cm.name),
-    (crewmembers: crewmember[]) => sortBy(crewmembers, (cm) => cm?.x),
-    (crewmembers: crewmember[]) => sortBy(crewmembers, (cm) => cm?.y),
-    (crewmembers: crewmember[]) => sortBy(crewmembers, (cm) => cm?.realZ),
-  ])(crewmembers);
-
   const tab: React.JSX.Element[] = [];
   // Data view
   // Please note, if you ever change the zoom values,
   // you MUST update styles/components/Tooltip.scss
   // and change the @for scss to match.
-  tab[0] = <CrewMonitorCrew crew={crew} />;
+  tab[0] = <CrewMonitorCrew crew={crewmembers} />;
 
   tab[1] = <CrewMonitorMapView zoom={props.zoom} onZoom={props.onZoom} />;
 
