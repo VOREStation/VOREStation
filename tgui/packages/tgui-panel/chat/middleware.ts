93b66490fbf8da4b9727b95a4f3efc09ac121de5
Kashargul <144968721+Kashargul@users.noreply.github.com>
2025-03-05 18:43:02 +0100

    fix that

--- tgui/packages/tgui-panel/chat/middleware.ts
+++ tgui/packages/tgui-panel/chat/middleware.ts
@@ -344,9 +344,10 @@ export const chatMiddleware = (store) => {
       type === importSettings.type
     ) {
       next(action);
+      const nextSettings = selectSettings(store.getState());
       chatRenderer.setHighlight(
-        settings.highlightSettings,
-        settings.highlightSettingById,
+        nextSettings.highlightSettings,
+        nextSettings.highlightSettingById,
       );
 
       return;
