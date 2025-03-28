3ddb38106fc7b7e4a1ba2e05683b6d67d9dc3174
Kashargul <144968721+Kashargul@users.noreply.github.com>
2025-03-28 10:32:01 +0100

    fix scroll tracking

--- tgui/packages/tgui-panel/chat/renderer.tsx
+++ tgui/packages/tgui-panel/chat/renderer.tsx
@@ -198,6 +198,7 @@ class ChatRenderer {
       }[]
     | null;
   databaseBackendEnabled: boolean;
+  lastScrollHeight: number;
   constructor() {
     /** @type {HTMLElement} */
     this.loaded = false;
@@ -227,13 +228,15 @@ class ChatRenderer {
     /** @type {HTMLElement} */
     this.scrollNode = null;
     this.scrollTracking = true;
+    this.lastScrollHeight = 0;
     this.handleScroll = (type) => {
       const node = this.scrollNode;
       if (node) {
         const height = node.scrollHeight;
         const bottom = node.scrollTop + node.offsetHeight;
         const scrollTracking =
-          Math.abs(height - bottom) < SCROLL_TRACKING_TOLERANCE;
+          Math.abs(height - bottom) < SCROLL_TRACKING_TOLERANCE ||
+          this.lastScrollHeight === 0;
         if (scrollTracking !== this.scrollTracking) {
           this.scrollTracking = scrollTracking;
           this.events.emit('scrollTrackingChanged', scrollTracking);
@@ -557,6 +560,10 @@ class ChatRenderer {
       return;
     }
     // Insert messages
+
+    if (this.scrollNode) {
+      this.lastScrollHeight = this.scrollNode.scrollHeight;
+    }
     const fragment = document.createDocumentFragment();
     const countByType = {};
     let node;
