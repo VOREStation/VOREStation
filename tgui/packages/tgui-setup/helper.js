5dd48b346648317ec03f203ab683ed931683ff71
Kashargul <144968721+Kashargul@users.noreply.github.com>
2025-04-05 04:22:31 +0200

    minify

--- tgui/packages/tgui-setup/helper.js
+++ tgui/packages/tgui-setup/helper.js
@@ -2,12 +2,11 @@
 
 (function () {
   // Utility functions
-  var hasOwn = Object.prototype.hasOwnProperty;
-
-  var assign = function (target) {
-    for (var i = 1; i < arguments.length; i++) {
-      var source = arguments[i];
-      for (var key in source) {
+  let hasOwn = Object.prototype.hasOwnProperty;
+  let assign = function (target) {
+    for (let i = 1; i < arguments.length; i++) {
+      let source = arguments[i];
+      for (let key in source) {
         if (hasOwn.call(source, key)) {
           target[key] = source[key];
         }
@@ -15,9 +14,8 @@
     }
     return target;
   };
-
-  var parseMetaTag = function (name) {
-    var content = document.getElementById(name).getAttribute('content');
+  let parseMetaTag = function (name) {
+    let content = document.getElementById(name).getAttribute('content');
     if (content === '[' + name + ']') {
       return null;
     }
@@ -27,7 +25,7 @@
   // BYOND API object
   // ------------------------------------------------------
 
-  var Byond = (window.Byond = {});
+  let Byond = (window.Byond = {});
 
   // Expose inlined metadata
   Byond.windowId = parseMetaTag('tgui:windowId');
@@ -37,25 +35,25 @@
 
   // Trident engine version
   Byond.TRIDENT = (function () {
-    var groups = navigator.userAgent.match(/Trident\/(\d+).+?;/i);
-    var majorVersion = groups && groups[1];
+    let groups = navigator.userAgent.match(/Trident\/(\d+).+?;/i);
+    let majorVersion = groups && groups[1];
     return majorVersion ? parseInt(majorVersion, 10) : null;
   })();
 
   // Blink engine version
   Byond.BLINK = (function () {
-    var groups = navigator.userAgent.match(/Chrome\/(\d+)\./);
-    var majorVersion = groups && groups[1];
+    let groups = navigator.userAgent.match(/Chrome\/(\d+)\./);
+    let majorVersion = groups && groups[1];
     return majorVersion ? parseInt(majorVersion, 10) : null;
   })();
 
   // Basic checks to detect whether this page runs in BYOND
-  var isByond =
+  let isByond =
     (Byond.TRIDENT !== null || Byond.BLINK !== null || window.cef_to_byond) &&
     location.hostname === '127.0.0.1' &&
     location.search !== '?external';
-  //As of BYOND 515 the path doesn't seem to include tmp dir anymore if you're trying to open tgui in external browser and looking why it doesn't work
-  //&& location.pathname.indexOf('/tmp') === 0
+  // As of BYOND 515 the path doesn't seem to include tmp dir anymore if you're trying to open tgui in external browser and looking why it doesn't work
+  // && location.pathname.indexOf('/tmp') === 0
 
   // Version constants
   Byond.IS_BYOND = isByond;
@@ -67,7 +65,7 @@
   Byond.__callbacks__ = [];
 
   // Reviver for BYOND JSON
-  var byondJsonReviver = function (key, value) {
+  let byondJsonReviver = function (key, value) {
     if (typeof value === 'object' && value !== null && value.__number__) {
       return parseFloat(value.__number__);
     }
@@ -82,15 +80,15 @@
       return;
     }
     // Build the URL
-    var url = (path || '') + '?';
-    var i = 0;
+    let url = (path || '') + '?';
+    let i = 0;
     if (params) {
-      for (var key in params) {
+      for (let key in params) {
         if (hasOwn.call(params, key)) {
           if (i++ > 0) {
             url += '&';
           }
-          var value = params[key];
+          let value = params[key];
           if (value === null || value === undefined) {
             value = '';
           }
@@ -112,7 +110,7 @@
     }
     // Send an HTTP request to DreamSeeker's HTTP server.
     // Allows sending much bigger payloads.
-    var xhr = new XMLHttpRequest();
+    let xhr = new XMLHttpRequest();
     xhr.open('GET', url);
     xhr.send();
   };
@@ -121,8 +119,8 @@
     if (!window.Promise) {
       throw new Error('Async calls require API level of ES2015 or later.');
     }
-    var index = Byond.__callbacks__.length;
-    var promise = new window.Promise(function (resolve) {
+    let index = Byond.__callbacks__.length;
+    let promise = new window.Promise((resolve) => {
       Byond.__callbacks__.push(resolve);
     });
     Byond.call(
@@ -148,14 +146,14 @@
     if (id === null) {
       id = '';
     }
-    var isArray = propName instanceof Array;
-    var isSpecific = propName && propName !== '*' && !isArray;
-    var promise = Byond.callAsync('winget', {
+    let isArray = propName instanceof Array;
+    let isSpecific = propName && propName !== '*' && !isArray;
+    let promise = Byond.callAsync('winget', {
       id: id,
       property: (isArray && propName.join(',')) || propName || '*',
     });
     if (isSpecific) {
-      promise = promise.then(function (props) {
+      promise = promise.then((props) => {
         return props[propName];
       });
     }
@@ -168,7 +166,7 @@
     } else if (typeof id === 'object') {
       return Byond.call('winset', id);
     }
-    var props = {};
+    let props = {};
     if (typeof propName === 'string') {
       props[propName] = propValue;
     } else {
@@ -186,13 +184,45 @@
     }
   };
 
+  let MAX_PACKET_SIZE = 1024;
+
   Byond.sendMessage = function (type, payload) {
-    var message =
+    let message =
       typeof type === 'string' ? { type: type, payload: payload } : type;
     // JSON-encode the payload
+
     if (message.payload !== null && message.payload !== undefined) {
       message.payload = JSON.stringify(message.payload);
+
+      if (!Byond.TRIDENT && message.payload.length > MAX_PACKET_SIZE) {
+        let chunks = [];
+
+        for (
+          let i = 0, charsLength = message.payload.length;
+          i < charsLength;
+          i += MAX_PACKET_SIZE
+        ) {
+          chunks.push(message.payload.substring(i, i + MAX_PACKET_SIZE));
+        }
+
+        for (let i = 0; i < chunks.length; i++) {
+          let to_send = chunks[i];
+
+          message = {
+            type: type,
+            packet: to_send,
+            packetId: i + 1,
+            totalPackets: chunks.length,
+            tgui: 1,
+            window_id: Byond.windowId,
+          };
+          Byond.topic(message);
+        }
+
+        return;
+      }
     }
+
     // Append an identifying header
     assign(message, {
       tgui: 1,
@@ -212,7 +242,7 @@
   };
 
   Byond.subscribeTo = function (type, listener) {
-    var _listener = function (_type, payload) {
+    let _listener = function (_type, payload) {
       if (_type === type) {
         listener(payload);
       }
@@ -224,45 +254,45 @@
   // Asset loaders
   // ------------------------------------------------------
 
-  var RETRY_ATTEMPTS = 5;
-  var RETRY_WAIT_INITIAL = 500;
-  var RETRY_WAIT_INCREMENT = 500;
+  let RETRY_ATTEMPTS = 5;
+  let RETRY_WAIT_INITIAL = 500;
+  let RETRY_WAIT_INCREMENT = 500;
 
-  var loadedAssetByUrl = {};
+  let loadedAssetByUrl = {};
 
-  var isStyleSheetLoaded = function (node, url) {
-    var styleSheet = node.sheet;
+  let isStyleSheetLoaded = function (node, url) {
+    let styleSheet = node.sheet;
     if (styleSheet) {
       return styleSheet.rules.length > 0;
     }
     return false;
   };
 
-  var injectNode = function (node) {
+  let injectNode = function (node) {
     if (!document.body) {
-      setTimeout(function () {
+      setTimeout(() => {
         injectNode(node);
       });
       return;
     }
-    var refs = document.body.childNodes;
-    var ref = refs[refs.length - 1];
+    let refs = document.body.childNodes;
+    let ref = refs[refs.length - 1];
     ref.parentNode.insertBefore(node, ref.nextSibling);
   };
 
-  var loadAsset = function (options) {
-    var url = options.url;
-    var type = options.type;
-    var sync = options.sync;
-    var attempt = options.attempt || 0;
+  let loadAsset = function (options) {
+    let url = options.url;
+    let type = options.type;
+    let sync = options.sync;
+    let attempt = options.attempt || 0;
     if (loadedAssetByUrl[url]) {
       return;
     }
     loadedAssetByUrl[url] = options;
     // Generic retry function
-    var retry = function () {
+    let retry = function () {
       if (attempt >= RETRY_ATTEMPTS) {
-        var errorMessage =
+        let errorMessage =
           'Error: Failed to load the asset ' +
           "'" +
           url +
@@ -276,7 +306,7 @@
         throw new Error(errorMessage);
       }
       setTimeout(
-        function () {
+        () => {
           loadedAssetByUrl[url] = null;
           options.attempt += 1;
           loadAsset(options);
@@ -286,7 +316,7 @@
     };
     // JS specific code
     if (type === 'js') {
-      var node = document.createElement('script');
+      let node = document.createElement('script');
       node.type = 'text/javascript';
       node.crossOrigin = 'anonymous';
       node.src = url;
@@ -306,7 +336,7 @@
     }
     // CSS specific code
     if (type === 'css') {
-      var node = document.createElement('link');
+      let node = document.createElement('link');
       node.type = 'text/css';
       node.rel = 'stylesheet';
       node.crossOrigin = 'anonymous';
@@ -316,7 +346,7 @@
       if (!sync) {
         node.media = 'only x';
       }
-      var removeNodeAndRetry = function () {
+      let removeNodeAndRetry = function () {
         node.parentNode.removeChild(node);
         node = null;
         retry();
@@ -354,10 +384,10 @@
     if (window.navigator.msSaveBlob) {
       window.navigator.msSaveBlob(blob, filename);
     } else if (window.showSaveFilePicker) {
-      var accept = {};
+      let accept = {};
       accept[blob.type] = [ext];
 
-      var opts = {
+      let opts = {
         suggestedName: filename,
         types: [
           {
@@ -367,17 +397,32 @@
         ],
       };
 
-      window
-        .showSaveFilePicker(opts)
-        .then(function (file) {
-          return file.createWritable();
-        })
-        .then(function (file) {
-          return file.write(blob).then(function () {
-            return file.close();
+      try {
+        window
+          .showSaveFilePicker(opts)
+          .then((fileHandle) => {
+            fileHandle
+              .createWritable()
+              .then((writeableFileHandle) => {
+                writeableFileHandle
+                  .write(blob)
+                  .then(() => {
+                    writeableFileHandle.close();
+                  })
+                  .catch((e) => {
+                    console.error(e);
+                  });
+              })
+              .catch((e) => {
+                console.error(e);
+              });
+          })
+          .catch((e) => {
+            console.error(e);
           });
-        })
-        .catch(function () {});
+      } catch (e) {
+        console.error(e);
+      }
     }
   };
 
@@ -391,7 +436,7 @@
 window.onerror = function (msg, url, line, col, error) {
   window.onerror.errorCount = (window.onerror.errorCount || 0) + 1;
   // Proper stacktrace
-  var stack = error && error.stack;
+  let stack = error && error.stack;
   // Ghetto stacktrace
   if (!stack) {
     stack = msg + '\n   at ' + url + ':' + line;
@@ -403,8 +448,8 @@ window.onerror = function (msg, url, line, col, error) {
   stack = window.__augmentStack__(stack, error);
   // Print error to the page
   if (Byond.strictMode) {
-    var errorRoot = document.getElementById('FatalError');
-    var errorStack = document.getElementById('FatalError__stack');
+    let errorRoot = document.getElementById('FatalError');
+    let errorStack = document.getElementById('FatalError__stack');
     if (errorRoot) {
       errorRoot.className = 'FatalError FatalError--visible';
       if (window.onerror.__stack__) {
@@ -412,11 +457,11 @@ window.onerror = function (msg, url, line, col, error) {
       } else {
         window.onerror.__stack__ = stack;
       }
-      var textProp = 'textContent';
+      let textProp = 'textContent';
       errorStack[textProp] = window.onerror.__stack__;
     }
     // Set window geometry
-    var setFatalErrorGeometry = function () {
+    let setFatalErrorGeometry = function () {
       Byond.winset(Byond.windowId, {
         titlebar: true,
         'is-visible': true,
@@ -451,7 +496,7 @@ window.onerror = function (msg, url, line, col, error) {
 
 // Catch unhandled promise rejections
 window.onunhandledrejection = function (e) {
-  var msg = 'UnhandledRejection';
+  let msg = 'UnhandledRejection';
   if (e.reason) {
     msg += ': ' + (e.reason.message || e.reason.description || e.reason);
     if (e.reason.stack) {
@@ -477,10 +522,10 @@ window.update = function (rawMessage) {
     return;
   }
   // Parse the message
-  var message = Byond.parseJson(rawMessage);
+  let message = Byond.parseJson(rawMessage);
   // Notify listeners
-  var listeners = window.update.listeners;
-  for (var i = 0; i < listeners.length; i++) {
+  let listeners = window.update.listeners;
+  for (let i = 0; i < listeners.length; i++) {
     listeners[i](message.type, message.payload);
   }
 };
@@ -494,23 +539,23 @@ window.update.flushQueue = function (listener) {
   if (window.update.queueActive) {
     window.update.queueActive = false;
     if (window.setTimeout) {
-      window.setTimeout(function () {
+      window.setTimeout(() => {
         window.update.queue = [];
       }, 0);
     }
   }
   // Process queued messages on provided listener
-  var queue = window.update.queue;
-  for (var i = 0; i < queue.length; i++) {
-    var message = Byond.parseJson(queue[i]);
+  let queue = window.update.queue;
+  for (let i = 0; i < queue.length; i++) {
+    let message = Byond.parseJson(queue[i]);
     listener(message.type, message.payload);
   }
 };
 
 window.replaceHtml = function (inline_html) {
-  var children = document.body.childNodes;
+  let children = document.body.childNodes;
 
-  for (var i = 0; i < children.length; i++) {
+  for (let i = 0; i < children.length; i++) {
     if (children[i].nodeValue == ' tgui:inline-html-start ') {
       while (children[i].nodeValue != ' tgui:inline-html-end ') {
         children[i].remove();
