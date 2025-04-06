ed399d0fdcea7979700ddfc5110c8039816d84f9
Kashargul <144968721+Kashargul@users.noreply.github.com>
2025-04-07 01:58:57 +0200

    initial

--- tgui/packages/tgui-bench/tests/Tooltip.test.tsx
+++ tgui/packages/tgui-bench/tests/Tooltip.test.tsx
@@ -1,10 +1,11 @@
+import React from 'react';
 import { createRenderer } from 'tgui/renderer';
 import { Box, Tooltip } from 'tgui-core/components';
 
 const render = createRenderer();
 
 export const ListOfTooltips = () => {
-  const nodes: JSX.Element[] = [];
+  const nodes: React.JSX.Element[] = [];
 
   for (let i = 0; i < 100; i++) {
     nodes.push(
