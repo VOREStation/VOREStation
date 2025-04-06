cc02a1a056245ec9bcbe58b186e6bf65b1363911
Kashargul <144968721+Kashargul@users.noreply.github.com>
2025-04-06 18:08:14 +0200

    split tgui html (#17476)
    * Repackage yarn sdks
    
    * oups forgot that
    
    * update dompurify
    
    * fix mantraps
    
    * .
    
    * fix weakrefs
    
    * rm
    
    * porting the HTML split
    
    * minify
    
    * ut that back
    
    * bye poly
    
    * .
    
    * re minify
    
    * ..
    
    * prettier
    
    * fix that

--- tgui/packages/tgui/interfaces/VolumePanel.tsx
+++ tgui/packages/tgui/interfaces/VolumePanel.tsx
@@ -1,6 +1,12 @@
 import { useBackend } from 'tgui/backend';
 import { Window } from 'tgui/layouts';
-import { Button, LabeledList, Section, Slider } from 'tgui-core/components';
+import {
+  Button,
+  LabeledList,
+  Section,
+  Slider,
+  Stack,
+} from 'tgui-core/components';
 
 type Data = {
   volume_channels: { key; val: number }[];
@@ -12,26 +18,34 @@ export const VolumePanel = (props) => {
   const { volume_channels } = data;
 
   return (
-    <Window width={350} height={600}>
+    <Window width={550} height={600}>
       <Window.Content>
-        <Section title="Volume Levels">
+        <Section title="Volume Levels" fill scrollable>
           <LabeledList>
             {Object.keys(volume_channels).map((key) => (
               <LabeledList.Item label={key} key={key}>
-                <Slider
-                  width="88%"
-                  minValue={0}
-                  maxValue={200}
-                  value={volume_channels[key] * 100}
-                  onChange={(e, val) =>
-                    act('adjust_volume', { channel: key, vol: val / 100 })
-                  }
-                />
-                <Button
-                  ml={1}
-                  icon="undo"
-                  onClick={() => act('adjust_volume', { channel: key, vol: 1 })}
-                />
+                <Stack>
+                  <Stack.Item grow>
+                    <Slider
+                      ml="1rem"
+                      minValue={0}
+                      maxValue={200}
+                      value={volume_channels[key] * 100}
+                      onChange={(e, val) =>
+                        act('adjust_volume', { channel: key, vol: val / 100 })
+                      }
+                    />
+                  </Stack.Item>
+                  <Stack.Item>
+                    <Button
+                      ml="1rem"
+                      icon="undo"
+                      onClick={() =>
+                        act('adjust_volume', { channel: key, vol: 1 })
+                      }
+                    />
+                  </Stack.Item>
+                </Stack>
               </LabeledList.Item>
             ))}
           </LabeledList>
