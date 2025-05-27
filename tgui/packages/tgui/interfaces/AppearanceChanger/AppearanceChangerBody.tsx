473015fdb16bea10b5c29de31257ae9263013768
C.L. <killer65311@gmail.com>
2025-05-27 15:52:05 -0400

    TGUI

--- tgui/packages/tgui/interfaces/AppearanceChanger/AppearanceChangerBody.tsx
+++ tgui/packages/tgui/interfaces/AppearanceChanger/AppearanceChangerBody.tsx
@@ -60,39 +60,13 @@ export const AppearanceChangerSpecies = (props) => {
             </Button>
             <ColorBox color={data.blood_color} mr={1} />
           </LabeledList.Item>
-          <LabeledList.Item label="Digitigrade">
-            <Button icon="pen" onClick={() => act('digitigrade')}>
-              {data.digitigrade ? 'Yes' : 'No'}
-            </Button>
-          </LabeledList.Item>
+
           <LabeledList.Item label="Species Sound">
             <Button icon="pen" onClick={() => act('species_sound')}>
               {data.species_sound}
             </Button>
           </LabeledList.Item>
         </LabeledList>
-      </Section>
-      <Section title="Sizing">
-        <LabeledList.Item label="Scale">
-          <Button icon="pen" onClick={() => act('size_scale')}>
-            {data.size_scale}
-          </Button>
-        </LabeledList.Item>
-        <LabeledList.Item label="Scale Appearance">
-          <Button icon="pen" onClick={() => act('scale_appearance')}>
-            {data.scale_appearance}
-          </Button>
-        </LabeledList.Item>
-        <LabeledList.Item label="Scale Offset">
-          <Button icon="pen" onClick={() => act('offset_override')}>
-            {data.offset_override}
-          </Button>
-        </LabeledList.Item>
-        <LabeledList.Item label="Weight">
-          <Button icon="pen" onClick={() => act('weight')}>
-            {data.weight}
-          </Button>
-        </LabeledList.Item>
       </Section>
     </Section>
   );
---
+++ tgui/packages/tgui/interfaces/AppearanceChanger/AppearanceChangerMisc.tsx
@@ -0,0 +1,44 @@
+import { useBackend } from 'tgui/backend';
+import { Button, LabeledList, Section, Stack } from 'tgui-core/components';
+
+import type { Data } from './types';
+
+export const AppearanceChangerMisc = (props) => {
+  const { act, data } = useBackend<Data>();
+
+  return (
+    <Section title="Misc" fill scrollable>
+      <Stack vertical fill>
+        <LabeledList.Item label="Digitigrade">
+          <Button icon="pen" onClick={() => act('digitigrade')}>
+            {data.digitigrade ? 'Yes' : 'No'}
+          </Button>
+        </LabeledList.Item>
+        <Section title="Scaling">
+          <LabeledList.Item label="Scale">
+            <Button icon="pen" onClick={() => act('size_scale')}>
+              {data.size_scale}
+            </Button>
+          </LabeledList.Item>
+          <LabeledList.Item label="Scale Offset">
+            <Button icon="pen" onClick={() => act('offset_override')}>
+              {data.offset_override}
+            </Button>
+          </LabeledList.Item>
+          <LabeledList.Item label="Scale Appearance">
+            <Button icon="pen" onClick={() => act('scale_appearance')}>
+              {data.scale_appearance}
+            </Button>
+          </LabeledList.Item>
+        </Section>
+        <Section title="Sizing">
+          <LabeledList.Item label="Weight">
+            <Button icon="pen" onClick={() => act('weight')}>
+              {data.weight}
+            </Button>
+          </LabeledList.Item>
+        </Section>
+      </Stack>
+    </Section>
+  );
+};
