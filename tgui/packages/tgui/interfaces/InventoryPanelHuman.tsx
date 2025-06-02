504365f2986e8bab1a2d91fb97f4e5d1bdeb7fa9
ShadowLarkens <shadowlarkens@gmail.com>
2025-06-02 05:34:32 -0700

    Port /tg/ stripping panel

--- tgui/packages/tgui/interfaces/InventoryPanelHuman.tsx
+++ tgui/packages/tgui/interfaces/InventoryPanelHuman.tsx
@@ -1,7 +1,9 @@
 import { useBackend } from 'tgui/backend';
 import { Window } from 'tgui/layouts';
-import { Button, LabeledList, Section } from 'tgui-core/components';
+import { Box, Button, Image, Section, Stack } from 'tgui-core/components';
 import type { BooleanLike } from 'tgui-core/react';
+
+import { resolveAsset } from '../assets';
 
 type Data = {
   slots: slot[];
@@ -19,8 +21,156 @@ type Data = {
 type slot = {
   name: string;
   item: string;
+  icon: string;
   act: string;
   params: { slot: number };
+};
+
+type GridInfo = {
+  gridRow: string;
+  gridColumn: string;
+};
+
+type SlotInfo = {
+  grid: GridInfo;
+  image: string;
+};
+
+const gridAt = (row: number, col: number): GridInfo => {
+  return { gridRow: `${row}`, gridColumn: `${col}` };
+};
+
+const KNOWN_SLOTS: Record<string, SlotInfo> = {
+  // Row 1
+  Glasses: {
+    grid: gridAt(1, 2),
+    image: 'inventory-glasses.png',
+  },
+  Hat: {
+    grid: gridAt(1, 3),
+    image: 'inventory-head.png',
+  },
+  'Left Ear': {
+    grid: gridAt(1, 4),
+    image: 'inventory-ears.png',
+  },
+  // Row 2
+  Mask: {
+    grid: gridAt(2, 3),
+    image: 'inventory-mask.png',
+  },
+  'Right Ear': {
+    grid: gridAt(2, 4),
+    image: 'inventory-ears.png',
+  },
+  // Row 3
+  Uniform: {
+    grid: gridAt(3, 2),
+    image: 'inventory-uniform.png',
+  },
+  Suit: {
+    grid: gridAt(3, 3),
+    image: 'inventory-suit.png',
+  },
+  Gloves: {
+    grid: gridAt(3, 4),
+    image: 'inventory-gloves.png',
+  },
+  'Left Hand': {
+    grid: gridAt(3, 5),
+    image: 'inventory-hand_l.png',
+  },
+  'Right Hand': {
+    grid: gridAt(3, 6),
+    image: 'inventory-hand_r.png',
+  },
+  // Row 4
+  Shoes: {
+    grid: gridAt(4, 3),
+    image: 'inventory-shoes.png',
+  },
+  // Row 5
+  ID: {
+    grid: gridAt(5, 1),
+    image: 'inventory-id.png',
+  },
+  'Suit Storage': {
+    grid: gridAt(5, 2),
+    image: 'inventory-suit_storage.png',
+  },
+  Belt: {
+    grid: gridAt(5, 3),
+    image: 'inventory-belt.png',
+  },
+  Back: {
+    grid: gridAt(5, 4),
+    image: 'inventory-back.png',
+  },
+};
+
+const getSlot = (slot: slot): SlotInfo => {
+  if (KNOWN_SLOTS[slot.name]) {
+    return KNOWN_SLOTS[slot.name];
+  }
+  return {
+    grid: gridAt(1, 1),
+    image: 'inventory-pocket.png',
+  };
+};
+
+export const SlotButton = (props: { slot: slot }) => {
+  const { slot } = props;
+  const { act } = useBackend();
+  const data = getSlot(slot);
+
+  return (
+    <Button
+      key={slot.name}
+      style={{ ...data.grid }}
+      width="64px"
+      height="64px"
+      position="relative"
+      onClick={() => act(slot.act, slot.params)}
+    >
+      <Stack
+        align="center"
+        justify="center"
+        position="absolute"
+        top={0}
+        left={0}
+        right={0}
+        bottom={0}
+      >
+        <Stack.Item>
+          <Image
+            fixErrors
+            src={resolveAsset(data.image)}
+            width="64px"
+            height="64px"
+          />
+        </Stack.Item>
+      </Stack>
+      {slot.icon ? (
+        <Stack
+          align="center"
+          justify="center"
+          position="absolute"
+          top={0}
+          left={0}
+          right={0}
+          bottom={0}
+        >
+          <Stack.Item>
+            <Image
+              src={`data:image/jpeg;base64,${slot.icon}`}
+              width="64px"
+              height="64px"
+            />
+          </Stack.Item>
+        </Stack>
+      ) : null}
+    </Button>
+  );
 };
 
 export const InventoryPanelHuman = (props) => {
@@ -39,38 +189,27 @@ export const InventoryPanelHuman = (props) => {
   } = data;
 
   return (
-    <Window width={400} height={600}>
+    <Window width={435} height={550}>
       <Window.Content scrollable>
         <Section>
-          <LabeledList>
-            {slots &&
-              slots.length &&
-              slots.map((slot) => (
-                <LabeledList.Item key={slot.name} label={slot.name}>
-                  <Button
-                    mb={-1}
-                    icon={slot.item ? 'hand-paper' : 'gift'}
-                    onClick={() => act(slot.act, slot.params)}
-                  >
-                    {slot.item || 'Nothing'}
-                  </Button>
-                </LabeledList.Item>
-              ))}
-            <LabeledList.Divider />
-            {specialSlots &&
-              specialSlots.length &&
-              specialSlots.map((slot) => (
-                <LabeledList.Item key={slot.name} label={slot.name}>
-                  <Button
-                    mb={-1}
-                    icon={slot.item ? 'hand-paper' : 'gift'}
-                    onClick={() => act(slot.act, slot.params)}
-                  >
-                    {slot.item || 'Nothing'}
-                  </Button>
-                </LabeledList.Item>
-              ))}
-          </LabeledList>
+          <Box
+            style={{
+              display: 'grid',
+              gridTemplateColumns: '1fr 1fr 1fr 1fr 1fr 1fr',
+              gridTemplateRows: '1fr 1fr 1fr 1fr 1fr',
+            }}
+            height="100%"
+            width="100%"
+          >
+            {slots?.length
+              ? slots.map((slot) => <SlotButton key={slot.name} slot={slot} />)
+              : null}
+            {specialSlots?.length
+              ? specialSlots.map((slot) => (
+                  <SlotButton key={slot.name} slot={slot} />
+                ))
+              : null}
+          </Box>
         </Section>
         <Section title="Actions">
           <Button
