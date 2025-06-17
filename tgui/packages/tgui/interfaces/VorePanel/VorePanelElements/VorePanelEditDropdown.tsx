7ecc812279859feec8edd96625da77bf33387e16
Kashargul <144968721+Kashargul@users.noreply.github.com>
2025-06-18 00:28:00 +0200

    .

--- tgui/packages/tgui/interfaces/VorePanel/VorePanelElements/VorePanelEditDropdown.tsx
+++ tgui/packages/tgui/interfaces/VorePanel/VorePanelElements/VorePanelEditDropdown.tsx
@@ -14,9 +14,9 @@ import type { DropdownEntry } from '../types';
 export const VorePanelEditDropdown = (props: {
   /** Switch between Element editing and display */
   editMode: boolean;
-  /** Our backend action on text area blur */
+  /** Our backend action on selection */
   action: string;
-  /** Our secondary backend action on text area blur */
+  /** Our secondary backend action on selection */
   subAction?: string;
   /** Our dropdown inputs and actions */
   options: (string | DropdownEntry)[];
