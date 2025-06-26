549313bd7c5a9ae1be3940994e383cc976467dee
Selis <12716288+ItsSelis@users.noreply.github.com>
2025-06-26 14:02:14 +0200

    1664 patch (#17907)
    * Patch for byond 1664
    
    * Rust
    
    * Rust
    
    * Adding librust_g.so to LFS
    
    * librust_g.so: convert to Git LFS
    
    * librust_g.so: convert to Git LFS
    
    * Migrate
    
    * Updated dependencies.sh & Fallback mirror for BYOND binaries
    
    * Checkout with LFS
    
    * Pulling LFS files
    
    * Verbose logging
    
    * More logging
    
    * Reverts verbose logging & adds step for byond dependencies
    
    * 🐚
    
    * 32
    
    * Replaces LFS with latest binary of rust-g release
    
    * Removes wrong file

--- tools/ci/install_byond.sh
+++ tools/ci/install_byond.sh
@@ -12,6 +12,15 @@ else
   mkdir -p "$HOME/BYOND"
   cd "$HOME/BYOND"
   curl -H "User-Agent: vstation/1.0 CI Script" "http://www.byond.com/download/build/${BYOND_MAJOR}/${BYOND_MAJOR}.${BYOND_MINOR}_byond_linux.zip" -o byond.zip
+  if [ $? -ne 0 ] || !(unzip -qt byond.zip); then
+    echo "Attempting fallback mirror..."
+    rm byond.zip
+    curl -H "User-Agent: vstation/1.0 CI Script" "https://spacestation13.github.io/byond-builds/${BYOND_MAJOR}/${BYOND_MAJOR}.${BYOND_MINOR}_byond_linux.zip" -o byond.zip
+    if [ $? -ne 0 ] || !(unzip -qt byond.zip); then
+      echo "Failure!"
+      exit 1
+    fi
+  fi
   unzip byond.zip
   rm byond.zip
   cd byond
