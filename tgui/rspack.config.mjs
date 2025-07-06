fad8ea3f1e9a1c0e778c4fdfac787edf299311de
Kashargul <144968721+Kashargul@users.noreply.github.com>
2025-07-06 06:03:55 +0200

    guess we'll try again

---
+++ tgui/rspack.config.mjs
@@ -0,0 +1,144 @@
+import path from 'node:path';
+
+import { defineConfig } from '@rspack/cli';
+import rspack from '@rspack/core';
+
+/**
+ * @param {boolean} verbose
+ * @returns  {import('@rspack/core').StatsOptions}
+ */
+export function createStats(verbose) {
+  return {
+    assets: verbose,
+    builtAt: verbose,
+    cached: false,
+    children: false,
+    chunks: false,
+    colors: true,
+    entrypoints: true,
+    hash: false,
+    modules: false,
+    performance: false,
+    timings: verbose,
+    version: verbose,
+  };
+}
+
+const dir = import.meta.dirname;
+
+export default defineConfig({
+  context: dir,
+  devtool: false,
+  entry: {
+    tgui: './packages/tgui',
+    'tgui-panel': './packages/tgui-panel',
+    'tgui-say': './packages/tgui-say',
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.([tj]s(x)?|cjs)$/,
+        type: 'javascript/auto',
+        use: [
+          {
+            loader: 'builtin:swc-loader',
+            options: {
+              jsc: {
+                parser: {
+                  syntax: 'typescript',
+                  tsx: true,
+                },
+                transform: {
+                  react: {
+                    runtime: 'automatic',
+                  },
+                },
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(s)?css$/,
+        type: 'javascript/auto',
+        use: [
+          rspack.CssExtractRspackPlugin.loader,
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              api: 'modern-compiler',
+              implementation: 'sass-embedded',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg)$/,
+        oneOf: [
+          {
+            issuer: /\.(s)?css$/,
+            type: 'asset/inline',
+          },
+          {
+            type: 'asset/resource',
+          },
+        ],
+        generator: {
+          filename: '[name][ext]',
+        },
+      },
+
+      {
+        test: /\.svg$/,
+        oneOf: [
+          {
+            issuer: /\.(s)?css$/,
+            type: 'asset/inline',
+          },
+          {
+            type: 'asset/resource',
+          },
+        ],
+        generator: {
+          filename: '[name][ext]',
+        },
+      },
+    ],
+  },
+  optimization: {
+    emitOnErrors: false,
+  },
+  output: {
+    path: 'public',
+    filename: '[name].bundle.js',
+    chunkFilename: '[name].bundle.js',
+    chunkLoadTimeout: 15000,
+    publicPath: '/',
+    assetModuleFilename: '[name][ext]',
+  },
+  performance: {
+    hints: false,
+  },
+  plugins: [
+    new rspack.CssExtractRspackPlugin({
+      chunkFilename: '[name].bundle.css',
+      filename: '[name].bundle.css',
+    }),
+    new rspack.EnvironmentPlugin({
+      NODE_ENV: 'production',
+    }),
+  ],
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx'],
+    alias: {
+      tgui: path.resolve(dir, './packages/tgui'),
+      'tgui-panel': path.resolve(dir, './packages/tgui-panel'),
+      'tgui-say': path.resolve(dir, './packages/tgui-say'),
+      'tgui-dev-server': path.resolve(dir, './packages/tgui-dev-server'),
+    },
+  },
+  stats: createStats(true),
+  target: ['web', 'browserslist:edge >= 123'],
+});
