8b098d25b33f12caafcfca81d76fddd54e7a33b1
Kashargul <144968721+Kashargul@users.noreply.github.com>
2025-08-23 00:10:41 +0200

    .

--- tgui/packages/tgui/interfaces/LogViewer/CategoryBar.tsx
+++ tgui/packages/tgui/interfaces/LogViewer/CategoryBar.tsx
@@ -5,6 +5,7 @@ import type { CategoryBarProps } from './types';
 
 export const CategoryBar = (props: CategoryBarProps) => {
   const { options, active, setActive } = props;
+
   const sorted = [...options].sort();
   const [categorySearch, setCategorySearch] = useState('');
 
