7996011a2f05fd0ea505b402d5a2aea313d152f8
Kashargul <144968721+Kashargul@users.noreply.github.com>
2025-08-27 23:29:48 +0200

    dependency up

--- dependencies.sh
+++ dependencies.sh
@@ -17,7 +17,7 @@ export RUST_G_VERSION=4.0.0
 export NODE_VERSION_LTS=22.14.0
 
 # Bun version
-export BUN_VERSION=1.2.20
+export BUN_VERSION=1.2.22
 
 # SpacemanDMM git tag
 export SPACEMAN_DMM_VERSION=suite-1.10
