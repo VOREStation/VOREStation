6e19e8178728028e3f1c5d6b37d0dcb1f0096e0b
Kashargul <144968721+Kashargul@users.noreply.github.com>
2025-09-26 17:24:46 +0200

    .

--- tgui/packages/tgui/interfaces/KeyComboModal.tsx
+++ tgui/packages/tgui/interfaces/KeyComboModal.tsx
@@ -1,9 +1,9 @@
 import { useRef, useState } from 'react';
 import { useBackend } from 'tgui/backend';
+import { Window } from 'tgui/layouts';
 import { Autofocus, Box, Button, Section, Stack } from 'tgui-core/components';
 import { isEscape, KEY } from 'tgui-core/keys';
 import type { BooleanLike } from 'tgui-core/react';
-import { Window } from '../layouts';
 import { InputButtons } from './common/InputButtons';
 import { Loader } from './common/Loader';
 
