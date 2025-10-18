f67baee2388cf78087aba279ff3267a76e1e6480
Kashargul <144968721+Kashargul@users.noreply.github.com>
2025-10-18 02:11:53 +0200

    moves robot sleepers to tgui

---
+++ tgui/packages/tgui/interfaces/RobotSleeper/SubElements/InjectorPanel.tsx
@@ -0,0 +1,27 @@
+import { useBackend } from 'tgui/backend';
+import { Button, Section, Stack } from 'tgui-core/components';
+import type { Data, RobotChem } from '../types';
+
+export const InjectorPanel = (props: { robotChems: RobotChem[] }) => {
+  const { act, data } = useBackend<Data>();
+  const { our_patient } = data;
+  const { robotChems } = props;
+
+  return (
+    <Section fill title="Injector">
+      <Stack wrap="wrap">
+        {robotChems.map((chem) => (
+          <Stack.Item basis="49%" key={chem.id}>
+            <Button
+              fluid
+              disabled={!our_patient || our_patient.stat === 2}
+              onClick={() => act('inject', { value: chem.id })}
+            >
+              Inject {chem.name}
+            </Button>
+          </Stack.Item>
+        ))}
+      </Stack>
+    </Section>
+  );
+};
