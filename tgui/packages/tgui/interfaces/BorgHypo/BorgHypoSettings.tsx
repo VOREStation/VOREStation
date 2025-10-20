af3b070f891d24b1fa48dbdd72d9a2d30393fdc5
Jenny <ghosttehspychecka@gmail.com>
2025-10-20 04:10:26 -0500

    tweaks settings UI to fit new chem settings changes

--- tgui/packages/tgui/interfaces/BorgHypo/BorgHypoSettings.tsx
+++ tgui/packages/tgui/interfaces/BorgHypo/BorgHypoSettings.tsx
@@ -1,12 +1,5 @@
 import { useBackend } from 'tgui/backend';
-import {
-  Button,
-  LabeledList,
-  Section,
-  Slider,
-  Stack,
-} from 'tgui-core/components';
-
+import { ChemDispenserSettings } from '../ChemDispenser/ChemDispenserSettings';
 import type { Data } from './types';
 
 export const BorgHypoSettings = (props) => {
@@ -14,43 +7,12 @@ export const BorgHypoSettings = (props) => {
   const { amount, minTransferAmount, maxTransferAmount, transferAmounts } =
     data;
   return (
-    <Section title="Settings" fill>
-      <LabeledList>
-        <LabeledList.Item label="Set Amount" verticalAlign="middle">
-          <Stack g={0.1}>
-            {transferAmounts.map((a, i) => (
-              <Stack.Item key={i}>
-                <Button
-                  textAlign="center"
-                  selected={amount === a}
-                  m="0"
-                  onClick={() =>
-                    act('set_amount', {
-                      amount: a,
-                    })
-                  }
-                >
-                  {`${a}u`}
-                </Button>
-              </Stack.Item>
-            ))}
-          </Stack>
-        </LabeledList.Item>
-        <LabeledList.Item label="Custom Amount">
-          <Slider
-            step={1}
-            stepPixelSize={5}
-            value={amount}
-            minValue={minTransferAmount}
-            maxValue={maxTransferAmount}
-            onChange={(e, value) =>
-              act('set_amount', {
-                amount: value,
-              })
-            }
-          />
-        </LabeledList.Item>
-      </LabeledList>
-    </Section>
+    <ChemDispenserSettings
+      selectedAmount={amount}
+      availableAmounts={transferAmounts}
+      minAmount={minTransferAmount}
+      maxAmount={maxTransferAmount}
+      amountAct={(amt) => act('set_amount', { amount: amt })}
+    />
   );
 };
