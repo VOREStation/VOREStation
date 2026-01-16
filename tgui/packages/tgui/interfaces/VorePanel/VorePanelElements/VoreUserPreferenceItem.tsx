4101f134bbfb99f970b200607f7b6ded346a56aa
Kashargul <144968721+Kashargul@users.noreply.github.com>
2026-01-16 16:28:06 +0100

    modern style

--- tgui/packages/tgui/interfaces/VorePanel/VorePanelElements/VoreUserPreferenceItem.tsx
+++ tgui/packages/tgui/interfaces/VorePanel/VorePanelElements/VoreUserPreferenceItem.tsx
@@ -1,15 +1,17 @@
+import type { ComponentProps } from 'react';
 import { useBackend } from 'tgui/backend';
-import { Button } from 'tgui-core/components';
-
+import { Button, type Floating } from 'tgui-core/components';
 import type { PreferenceData } from '../types';
 
 export const VoreUserPreferenceItem = (props: {
+  /** The preference data object */
   spec: PreferenceData;
-  [rest: string]: any;
+  /** Position of the tooltip */
+  tooltipPosition: ComponentProps<typeof Floating>['placement'];
 }) => {
   const { act } = useBackend();
 
-  const { spec, ...rest } = props;
+  const { spec, tooltipPosition } = props;
   const { action, test, tooltip, content, fluid = true, back_color } = spec;
 
   return (
@@ -22,7 +24,7 @@ export const VoreUserPreferenceItem = (props: {
         back_color ? (test ? back_color.enabled : back_color.disabled) : ''
       }
       tooltip={`${tooltip.main} ${test ? tooltip.disable : tooltip.enable}`}
-      {...rest}
+      tooltipPosition={tooltipPosition}
     >
       {test ? content.enabled : content.disabled}
     </Button>
