78dee156a63a8e2871382fbfaffd4938748f38c7
Kashargul <144968721+Kashargul@users.noreply.github.com>
2026-01-22 20:50:02 +0100

    adds spoilter to character Directory

---
+++ tgui/packages/tgui/interfaces/CharacterDirectory/CharacterDirectorySpoiler.tsx
@@ -0,0 +1,22 @@
+import { Box } from 'tgui-core/components';
+
+export const SpoilerText = (props: { text: string }) => {
+  const { text } = props;
+
+  const parts = text.split('||');
+
+  return (
+    <>
+      {parts.map((part, index) => (
+        <Box
+          inline
+          preserveWhitespace
+          key={index}
+          className={index % 2 === 1 ? 'spoiler' : undefined}
+        >
+          {part}
+        </Box>
+      ))}
+    </>
+  );
+};
