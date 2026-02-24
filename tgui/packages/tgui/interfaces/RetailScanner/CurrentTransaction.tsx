f811ad84be9bb3e6acd4801298aedc8dd0a1b08f
Kashargul <144968721+Kashargul@users.noreply.github.com>
2026-02-24 01:05:42 +0100

    retail scanner

---
+++ tgui/packages/tgui/interfaces/RetailScanner/CurrentTransaction.tsx
@@ -0,0 +1,101 @@
+import { useBackend } from 'tgui/backend';
+import {
+  Button,
+  Divider,
+  NumberInput,
+  Section,
+  Stack,
+  Table,
+} from 'tgui-core/components';
+import type { Data } from './types';
+
+export const CurrentTransaction = (model) => {
+  const { act, data } = useBackend<Data>();
+  const { current_transactioon, locked } = data;
+
+  const { items, prices } = current_transactioon;
+
+  return (
+    <Section fill scrollable title="Active Transaction">
+      <Table>
+        <Table.Row>
+          <Table.Cell header>New Transaction</Table.Cell>
+          <Table.Cell collapsing>
+            <Button.Confirm
+              disabled={locked}
+              color="red"
+              onClick={() => act('clear_entry')}
+            >
+              Clear Entry
+            </Button.Confirm>
+          </Table.Cell>
+        </Table.Row>
+        <Divider />
+        {items && prices && (
+          <>
+            {' '}
+            {Object.keys(items).map((item) => (
+              <Table.Row key={item}>
+                <Table.Cell>
+                  <Stack>
+                    <Stack.Item grow>
+                      {locked ? (
+                        `${items[item]} x`
+                      ) : (
+                        <NumberInput
+                          width="20px"
+                          minValue={0}
+                          maxValue={20}
+                          value={items[item]}
+                          onChange={(value) =>
+                            act('set_amount', { item: item, amount: value })
+                          }
+                          unit="x"
+                        />
+                      )}
+                      {` ${item}`}
+                    </Stack.Item>
+                    {!locked && (
+                      <>
+                        <Stack.Item>
+                          <Button onClick={() => act('add', { item: item })}>
+                            +
+                          </Button>
+                        </Stack.Item>
+                        <Stack.Item>
+                          <Button
+                            onClick={() => act('subtract', { item: item })}
+                          >
+                            -
+                          </Button>
+                        </Stack.Item>
+                        <Stack.Item>
+                          <Button.Confirm
+                            color="red"
+                            icon="trash"
+                            onClick={() => act('clear', { item: item })}
+                          />
+                        </Stack.Item>
+                      </>
+                    )}
+                  </Stack>
+                </Table.Cell>
+                <Table.Cell collapsing>
+                  {`${prices[item] * items[item]} ₮`}
+                </Table.Cell>
+              </Table.Row>
+            ))}
+            <Table.Row>
+              <Table.Cell textAlign="right" color="label">
+                Total Amount
+              </Table.Cell>
+              <Table.Cell
+                collapsing
+              >{`${current_transactioon.amount} ₮`}</Table.Cell>
+            </Table.Row>
+          </>
+        )}
+      </Table>
+    </Section>
+  );
+};
