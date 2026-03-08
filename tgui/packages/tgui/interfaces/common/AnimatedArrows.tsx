a0b7ab175e6ef5739f0c7248a284a6b4fd4902f5
Kashargul <144968721+Kashargul@users.noreply.github.com>
2026-03-09 00:29:55 +0100

    fix spacebattle (#19240)
    * fix spacebattle
    
    * clean that up
    
    * .
    
    * eh
    
    * fine
    
    * .

--- tgui/packages/tgui/interfaces/common/AnimatedArrows.tsx
+++ tgui/packages/tgui/interfaces/common/AnimatedArrows.tsx
@@ -35,3 +35,38 @@ export const AnimatedArrows = (
     </Box>
   );
 };
+
+export const AnimatedArrowsLeft = (
+  props: { on: boolean } & ComponentProps<typeof Box>,
+) => {
+  const { on, ...rest } = props;
+
+  const [activeArrow, setActiveArrow] = useState(0);
+
+  // Lower to make it animate faster
+  const SPEED = 200;
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setActiveArrow((arrow) => (arrow - 1 + 3) % 3);
+    }, SPEED);
+    return () => clearInterval(id);
+  }, []);
+
+  return (
+    <Box {...rest}>
+      <Icon
+        color={!on ? 'gray' : activeArrow === 0 ? 'green' : 'white'}
+        name="chevron-left"
+      />
+      <Icon
+        color={!on ? 'gray' : activeArrow === 1 ? 'green' : 'white'}
+        name="chevron-left"
+      />
+      <Icon
+        color={!on ? 'gray' : activeArrow === 2 ? 'green' : 'white'}
+        name="chevron-left"
+      />
+    </Box>
+  );
+};
