7901c9a4c917dc57f42261435697fc9633d8fc39
Kashargul <144968721+Kashargul@users.noreply.github.com>
2026-03-11 16:12:40 +0100

    stack gap handles that

--- tgui/packages/tgui/interfaces/AnomalyHarvester/Sample.tsx
+++ tgui/packages/tgui/interfaces/AnomalyHarvester/Sample.tsx
@@ -8,41 +8,37 @@ export const SampleDisplay = (props) => {
 
   return (
     <Section fill scrollable title="Generated Anomalous Material">
-      <Stack vertical fill g={0}>
+      <Stack vertical fill>
         {samples.length > 0 &&
           samples.map((sample, index) => {
             return (
-              <Stack.Item key={index} grow>
-                <Box
-                  style={{
-                    marginBottom: '0.5rem',
-                    padding: '0.5rem',
-                    border: '1px solid #ccc',
-                  }}
-                >
-                  <Stack>
-                    <Stack.Item>
-                      <DmIcon
-                        icon={sample.icon}
-                        icon_state={sample.icon_state}
-                      />
-                    </Stack.Item>
-                    <Stack.Item grow>
-                      <Box style={{ paddingLeft: '0.5rem' }}>{sample.name}</Box>
-                    </Stack.Item>
-                    <Stack.Item>
-                      <Button
-                        onClick={() =>
-                          act('release_sample', {
-                            ref: sample.ref,
-                          })
-                        }
-                      >
-                        Release
-                      </Button>
-                    </Stack.Item>
-                  </Stack>
-                </Box>
+              <Stack.Item
+                key={index}
+                grow
+                style={{
+                  padding: '0.5rem',
+                  border: '1px solid #ccc',
+                }}
+              >
+                <Stack>
+                  <Stack.Item>
+                    <DmIcon icon={sample.icon} icon_state={sample.icon_state} />
+                  </Stack.Item>
+                  <Stack.Item grow>
+                    <Box style={{ paddingLeft: '0.5rem' }}>{sample.name}</Box>
+                  </Stack.Item>
+                  <Stack.Item>
+                    <Button
+                      onClick={() =>
+                        act('release_sample', {
+                          ref: sample.ref,
+                        })
+                      }
+                    >
+                      Release
+                    </Button>
+                  </Stack.Item>
+                </Stack>
               </Stack.Item>
             );
           })}
