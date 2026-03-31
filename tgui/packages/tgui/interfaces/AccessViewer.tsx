af1a6a26911aa9d5c1b745057733a40518a7c56b
Will <7099514+Willburd@users.noreply.github.com>
2026-03-31 12:20:34 -0400

    basic ui

---
+++ tgui/packages/tgui/interfaces/AccessViewer.tsx
@@ -0,0 +1,60 @@
+import { useBackend } from 'tgui/backend';
+import { Window } from 'tgui/layouts';
+import { Button, LabeledList, Section } from 'tgui-core/components';
+import type { BooleanLike } from 'tgui-core/react';
+
+type Data = {
+  access_list: Access[];
+  name: string;
+  coords: string;
+  req_access: number[];
+  req_one_access: number[];
+};
+
+type Access = {
+  name: string;
+  id: number;
+  has_req: BooleanLike;
+  has_req_one: BooleanLike;
+};
+
+export const AccessViewer = (props) => {
+  const { act, data } = useBackend<Data>();
+  const { access_list, name, coords, req_access, req_one_access } = data;
+
+  access_list.map((entry) => {
+    entry.has_req = req_access.includes(entry.id);
+    entry.has_req_one = req_one_access.includes(entry.id);
+  });
+
+  return (
+    <Window width={520} height={540}>
+      <Window.Content scrollable>
+        <Section title={`${name}'s access`}>
+          <LabeledList>
+            <LabeledList.Item label="Coords">{coords}</LabeledList.Item>
+            {access_list.map((entry) => (
+              <LabeledList.Item
+                label={`${entry.id}: ${entry.name}`}
+                key={entry.id}
+              >
+                <Button
+                  color={entry.has_req ? 'good' : 'bad'}
+                  onClick={() => act('req_all', { set_id: entry.id })}
+                >
+                  Required
+                </Button>
+                <Button
+                  color={entry.has_req_one ? 'good' : 'bad'}
+                  onClick={() => act('req_one', { set_id: entry.id })}
+                >
+                  Have Any
+                </Button>
+              </LabeledList.Item>
+            ))}
+          </LabeledList>
+        </Section>
+      </Window.Content>
+    </Window>
+  );
+};
