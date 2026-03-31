ad0b03fdcb7b0217075d58d08682f2122f871bde
Will <7099514+Willburd@users.noreply.github.com>
2026-03-31 08:20:56 -0400

    tuned

--- tgui/packages/tgui/interfaces/DopplerArray/functions.ts
+++ tgui/packages/tgui/interfaces/DopplerArray/functions.ts
@@ -2,7 +2,9 @@ import { explosionTypes } from './constants';
 import type { Explosion } from './types';
 
 export function getSeverity(exp: Explosion) {
-  if (exp.devastation_range > 5) return explosionTypes[2];
-  if (exp.devastation_range > 2) return explosionTypes[1];
+  if (exp.devastation_range >= 3 || exp.heavy_impact_range >= 5)
+    return explosionTypes[2];
+  if (exp.devastation_range >= 2 || exp.heavy_impact_range >= 3)
+    return explosionTypes[1];
   return explosionTypes[0];
 }
