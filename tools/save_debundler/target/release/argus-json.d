306d524273b3219ba133335ee9ae9fb1d0b8035d
ARGUS <268038739+ARGUS-Memory@users.noreply.github.com>
2026-04-02 10:06:57 +0200

    Add Git Ignore

--- tools/save_debundler/target/release/argus-json.d
+++
@@ -1,1 +0,0 @@
-C:\Hive\SS13\ Research\VOREStation\tools\save_debundler\target\release\argus-json.exe: C:\Hive\SS13\ Research\VOREStation\tools\save_debundler\src\main.rs
--- tools/save_debundler/target/release/deps/argus_json.d
+++
@@ -1,5 +0,0 @@
-C:\Hive\SS13 Research\VOREStation\tools\save_debundler\target\release\deps\argus_json.d: src\main.rs
-
-C:\Hive\SS13 Research\VOREStation\tools\save_debundler\target\release\deps\argus_json.exe: src\main.rs
-
-src\main.rs:
