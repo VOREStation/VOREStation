66e0b9764dcc5a9fc49c3efb20be3b83849b239d
Kashargul <144968721+Kashargul@users.noreply.github.com>
2026-04-05 02:55:01 +0200

    consts

--- tgui/packages/tgui/interfaces/AccessViewer/AccessTypeDisplay.tsx
+++ tgui/packages/tgui/interfaces/AccessViewer/AccessTypeDisplay.tsx
@@ -4,22 +4,22 @@ import { ACCESS_FLAGS } from './constants';
 export const AccessTypeDisplay = (props: { type: number }) => {
   const { type } = props;
 
-  const names: string[] = ACCESS_FLAGS.filter(
-    (flag) => (type & flag.bit) !== 0,
-  ).map((flag) => flag.label);
-
+  const names = ACCESS_FLAGS.filter((flag) => type & flag.bit).map(
+    (f) => f.label,
+  );
   const allFlags = ACCESS_FLAGS.reduce((acc, f) => acc | f.bit, 0);
 
-  let label = '';
-  let tooltip: string | undefined;
-  if (names.length === 0) label = 'None';
-  else if (type === allFlags) {
-    label = 'All';
-    tooltip = names.join(', ');
-  } else if (names.length > 1) {
-    label = 'Multiple';
-    tooltip = names.join(', ');
-  } else label = names[0];
+  const label =
+    names.length === 0
+      ? 'None'
+      : type === allFlags
+        ? 'All'
+        : names.length > 1
+          ? 'Multiple'
+          : names[0];
+
+  const tooltip =
+    names.length > 1 || type === allFlags ? names.join(', ') : undefined;
 
   return (
     <Tooltip content={tooltip}>
