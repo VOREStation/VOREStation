c116088213bfe278abd4a8e1c139f519f65b8127
Will <7099514+Willburd@users.noreply.github.com>
2026-04-08 12:06:09 -0400

    Check Access List Mapper Verb (#19357)
    * access dev tool
    
    * debug
    
    * basic ui
    
    * invalid ids
    
    * fix start
    
    * lets make that a unittest instead
    
    * oop
    
    * test that too
    
    * oop
    
    * death
    
    * cleanup
    
    * trying to sublist
    
    * no idea what i am doing
    
    * more stuff
    
    * ugh
    
    * .
    
    * labels
    
    * consts
    
    * .
    
    ---------
    
    Co-authored-by: Kashargul <144968721+Kashargul@users.noreply.github.com>

---
+++ tgui/packages/tgui/interfaces/AccessViewer/index.tsx
@@ -0,0 +1,106 @@
+import { Fragment, useState } from 'react';
+import { useBackend } from 'tgui/backend';
+import { Window } from 'tgui/layouts';
+import { Button, Divider, Input, Section, Table } from 'tgui-core/components';
+import { createSearch } from 'tgui-core/string';
+import { AccessTypeDisplay } from './AccessTypeDisplay';
+import { region_ids } from './constants';
+import type { Access, Data } from './types';
+
+export const AccessViewer = (props) => {
+  const { act, data } = useBackend<Data>();
+  const { access_list, name, coords, req_access, req_one_access } = data;
+  const [searchText, setSearchText] = useState('');
+
+  const access_regions: Record<number, Access[]> = {
+    [region_ids.None]: [],
+    [region_ids.Security]: [],
+    [region_ids.Medbay]: [],
+    [region_ids.Research]: [],
+    [region_ids.Engineering]: [],
+    [region_ids.Command]: [],
+    [region_ids.General]: [],
+    [region_ids.Supply]: [],
+  };
+
+  const search = createSearch<Access>(searchText.toLowerCase(), (entry) =>
+    `${entry.id}: ${entry.name}`.toLowerCase(),
+  );
+
+  access_list.forEach((entry) => {
+    entry.has_req = req_access?.includes(entry.id);
+    entry.has_req_one = req_one_access?.includes(entry.id);
+
+    if (search(entry)) {
+      access_regions[entry.region].push(entry);
+    }
+  });
+
+  return (
+    <Window width={620} height={540}>
+      <Window.Content>
+        <Section
+          fill
+          title={`${name}'s access at ${coords}`}
+          scrollable
+          buttons={
+            <Input
+              width="200px"
+              value={searchText}
+              onChange={setSearchText}
+              placeholder="Search for access..."
+            />
+          }
+        >
+          <Table>
+            {Object.keys(access_regions).map((key, index) => (
+              <Fragment key={key}>
+                {index > 0 && (
+                  <Table.Row>
+                    <Table.Cell colSpan={4}>
+                      <Divider />
+                    </Table.Cell>
+                  </Table.Row>
+                )}
+                <Table.Row header>
+                  <Table.Cell colSpan={4}>{region_ids[key]}</Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                  <Table.Cell colSpan={4}>
+                    <Divider />
+                  </Table.Cell>
+                </Table.Row>
+                {access_regions[key].map((entry: Access) => (
+                  <Table.Row key={entry.id}>
+                    <Table.Cell
+                      collapsing
+                    >{`${entry.id}: ${entry.name}`}</Table.Cell>
+                    <Table.Cell collapsing>
+                      <Button.Checkbox
+                        checked={entry.has_req}
+                        onClick={() => act('req_all', { set_id: entry.id })}
+                      >
+                        Requires
+                      </Button.Checkbox>
+                    </Table.Cell>
+                    <Table.Cell collapsing>
+                      <Button.Checkbox
+                        checked={entry.has_req_one}
+                        onClick={() => act('req_one', { set_id: entry.id })}
+                      >
+                        Have Any
+                      </Button.Checkbox>
+                    </Table.Cell>
+                    <Table.Cell collapsing color="label">
+                      <AccessTypeDisplay type={entry.access_type} />
+                    </Table.Cell>
+                  </Table.Row>
+                ))}
+              </Fragment>
+            ))}
+          </Table>
+        </Section>
+      </Window.Content>
+    </Window>
+  );
+};
