40a6b5af887a6f854a4a25e765cb414eb0d6462d
Kashargul <144968721+Kashargul@users.noreply.github.com>
2026-06-09 23:52:40 +0200

    use ref. if really needed

--- tgui/packages/tgui/interfaces/Pda/pda_screens/pda_depgoals.tsx
+++ tgui/packages/tgui/interfaces/Pda/pda_screens/pda_depgoals.tsx
@@ -15,7 +15,7 @@ type Goal = {
   name: string;
   description: string;
   count: number;
-  index: number;
+  ref: string;
 };
 
 export const pda_depgoals = (props) => {
@@ -25,9 +25,11 @@ export const pda_depgoals = (props) => {
 
   return (
     <Box>
-      {(!goals.length && (
+      {!goals.length ? (
         <NoticeBox>No departmental goals found!</NoticeBox>
-      )) || <GoalsDisplay />}
+      ) : (
+        <GoalsDisplay />
+      )}
     </Box>
   );
 };
@@ -38,35 +40,34 @@ const GoalsDisplay = (props) => {
 
   return (
     <Stack vertical fill>
-      {goals.length > 0 &&
-        goals.map((goal) => (
-          <Section key={goal.index}>
-            <Stack vertical>
-              <Stack.Item>
-                <Box fontSize="4">{goal.name}</Box>
-                <Box fontSize="2" color="gray">
-                  {goal.description}
-                </Box>
-              </Stack.Item>
-              <Stack.Item>
-                <ProgressBar
-                  value={goal.count}
-                  minValue={0}
-                  maxValue={100}
-                  ranges={{
-                    good: [85, 100],
-                    average: [25, 85],
-                    bad: [0, 25],
-                  }}
-                >
-                  <Stack align="baseline">
-                    {!!goal.name && <Stack.Item grow>{goal.count}%</Stack.Item>}
-                  </Stack>
-                </ProgressBar>
-              </Stack.Item>
-            </Stack>
-          </Section>
-        ))}
+      {goals.map((goal) => (
+        <Section key={goal.ref}>
+          <Stack vertical>
+            <Stack.Item>
+              <Box fontSize="4">{goal.name}</Box>
+              <Box fontSize="2" color="gray">
+                {goal.description}
+              </Box>
+            </Stack.Item>
+            <Stack.Item>
+              <ProgressBar
+                value={goal.count}
+                minValue={0}
+                maxValue={100}
+                ranges={{
+                  good: [85, 100],
+                  average: [25, 85],
+                  bad: [0, 25],
+                }}
+              >
+                <Stack align="baseline">
+                  <Stack.Item grow>{goal.count}%</Stack.Item>
+                </Stack>
+              </ProgressBar>
+            </Stack.Item>
+          </Stack>
+        </Section>
+      ))}
     </Stack>
   );
 };
