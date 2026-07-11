12be29d5ed4fd7fe5c49b5d4675af8bad3ff797f
Will <7099514+Willburd@users.noreply.github.com>
2026-07-11 09:02:39 -0400

    don't mess up biome

--- tgui/packages/tgui/interfaces/ResleevingConsole/ResleevingConsoleCoreDump.tsx
+++ tgui/packages/tgui/interfaces/ResleevingConsole/ResleevingConsoleCoreDump.tsx
@@ -1,94 +1,94 @@
-import { useBackend } from "tgui/backend";
+import { useBackend } from 'tgui/backend';
 import {
-	Box,
-	Button,
-	Dimmer,
-	Icon,
-	ProgressBar,
-	Stack,
-} from "tgui-core/components";
-import type { Data } from "./types";
+  Box,
+  Button,
+  Dimmer,
+  Icon,
+  ProgressBar,
+  Stack,
+} from 'tgui-core/components';
+import type { Data } from './types';
 
 export const ResleevingConsoleCoreDump = (props) => {
-	return (
-		<Dimmer>
-			<Stack direction="column" justify="space-evenly" align="center">
-				<Stack.Item grow>
-					<Icon size={12} color="bad" name="exclamation-triangle" />
-				</Stack.Item>
-				<Stack.Item grow color="bad" mt={5}>
-					<h2>TransCore dump completed. Resleeving offline.</h2>
-				</Stack.Item>
-			</Stack>
-		</Dimmer>
-	);
+  return (
+    <Dimmer>
+      <Stack direction="column" justify="space-evenly" align="center">
+        <Stack.Item grow>
+          <Icon size={12} color="bad" name="exclamation-triangle" />
+        </Stack.Item>
+        <Stack.Item grow color="bad" mt={5}>
+          <h2>TransCore dump completed. Resleeving offline.</h2>
+        </Stack.Item>
+      </Stack>
+    </Dimmer>
+  );
 };
 
 export const ResleevingConsoleDiskPrep = (props) => {
-	const { act } = useBackend();
-	return (
-		<Dimmer textAlign="center">
-			<Box color="bad">
-				<h1>TRANSCORE DUMP</h1>
-			</Box>
-			<Box color="bad">
-				<h2>!!WARNING!!</h2>
-			</Box>
-			<PulsingWarningTriangle />
-			<Box color="bad">
-				This will transfer all minds to the dump disk, and the TransCore will be
-				made unusable until post-shift maintenance! This should only be used in
-				emergencies!
-			</Box>
-			<Box mt={4}>
-				<Button icon="eject" color="good" onClick={() => act("ejectdisk")}>
-					Eject Disk
-				</Button>
-			</Box>
-			<Box mt={4}>
-				<Button.Confirm
-					icon="exclamation-triangle"
-					confirmIcon="exclamation-triangle"
-					confirmContent="Disable Transcore?"
-					color="bad"
-					onClick={() => act("coredump")}
-				>
-					Core Dump
-				</Button.Confirm>
-			</Box>
-		</Dimmer>
-	);
+  const { act } = useBackend();
+  return (
+    <Dimmer textAlign="center">
+      <Box color="bad">
+        <h1>TRANSCORE DUMP</h1>
+      </Box>
+      <Box color="bad">
+        <h2>!!WARNING!!</h2>
+      </Box>
+      <PulsingWarningTriangle />
+      <Box color="bad">
+        This will transfer all minds to the dump disk, and the TransCore will be
+        made unusable until post-shift maintenance! This should only be used in
+        emergencies!
+      </Box>
+      <Box mt={4}>
+        <Button icon="eject" color="good" onClick={() => act('ejectdisk')}>
+          Eject Disk
+        </Button>
+      </Box>
+      <Box mt={4}>
+        <Button.Confirm
+          icon="exclamation-triangle"
+          confirmIcon="exclamation-triangle"
+          confirmContent="Disable Transcore?"
+          color="bad"
+          onClick={() => act('coredump')}
+        >
+          Core Dump
+        </Button.Confirm>
+      </Box>
+    </Dimmer>
+  );
 };
 
 export const PulsingWarningTriangle = (props) => {
-	return (
-		<Icon
-			color="bad"
-			size={4}
-			name="exclamation-triangle"
-			className="ResleevingConsole__WarningTriangle"
-		/>
-	);
+  return (
+    <Icon
+      color="bad"
+      size={4}
+      name="exclamation-triangle"
+      className="ResleevingConsole__WarningTriangle"
+    />
+  );
 };
 
 export const ResleevingConsoleDumpProgress = (props) => {
-	const { act, data } = useBackend<Data>();
-	const { dump_progress } = data;
-	return (
-		<Dimmer textAlign="center">
-			<Box color="bad">
-				<h1>TRANSCORE DUMP IN PROGRESS</h1>
-			</Box>
-			<Box color="bad">
-				<h2>!!WARNING!!</h2>
-			</Box>
-			<PulsingWarningTriangle />
-			<ProgressBar value={1 - dump_progress} maxValue={1} color="bad" />
-			<Box mt={4}>
-				<Button icon="eject" color="good" onClick={() => act("ejectdisk")}>
-					Eject Disk
-				</Button>
-			</Box>
-		</Dimmer>
-	);
+  const { act, data } = useBackend<Data>();
+  const { dump_progress } = data;
+  return (
+    <Dimmer textAlign="center">
+      <Box color="bad">
+        <h1>TRANSCORE DUMP IN PROGRESS</h1>
+      </Box>
+      <Box color="bad">
+        <h2>!!WARNING!!</h2>
+      </Box>
+      <PulsingWarningTriangle />
+      <ProgressBar value={1 - dump_progress} maxValue={1} color="bad" />
+      <Box mt={4}>
+        <Button icon="eject" color="good" onClick={() => act('ejectdisk')}>
+          Eject Disk
+        </Button>
+      </Box>
+    </Dimmer>
+  );
 };
